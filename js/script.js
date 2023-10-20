12176ab0a3299afef1f38388797a61dbab96fd06
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-20 10:47:49 +0200

    updated scoreCounter, updated css

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 //creiamo una const che conterra il numero delle bombe
 //creiamo un array vuoto dove metteremo i numeri casuali(bombe sparse)
 //creiamo una funziona che genera le bombe
+"use strict";
 
 let score = 0;
 //il nostro numero di bombe
@@ -18,19 +19,19 @@ const NUM_BOMBS = 16;
     
     scoreCounter = document.getElementById('score');
     const playGround = document.getElementById('playground');
-    
+    score = 0;
     playGround.innerHTML = '';
-    scoreCounter.innerHTML = '';
+    scoreCounter.innerHTML = '0000';
     
     let numSquare;
-   
+    
     //usiamo il ciclo for per stampare il numero di quadratini corrente
     if (selectLevel.value === 'easy'){
         numSquare = 100;
     } else if (selectLevel.value === 'medium'){
-        numSquare = 90;
+        numSquare = 81;
     } else if (selectLevel.value === 'hard'){
-        numSquare = 42;
+        numSquare = 49;
     } 
 
     let bombs = bombsGenerator(numSquare);
@@ -59,20 +60,21 @@ function drawSquare(squareIndex, numSquare, bombs){
     //aggiungiamo evento per quando clicchiamo sul quadratino
     square.addEventListener('click', function(){
        
-        //evita più click sulal cella
+        //evita più click sulle celle
         if (square.classList.contains('bg-primary')) return;
-
+        
         square.classList.add('bg-primary')
         square.style.color = 'white';
         console.log('hai cliccato sul numero: ', this.textContent);
         updateScore();
+        //se clicco sulla bomba
         if(bombs.includes(parseInt(squareIndex +1))){
             square.classList.add('bg-danger');
             square.innerHTML = '<i class="fa-solid fa-bomb fa-beat"></i>';
             alert(`Hai perso! il tuo punteggio è 000${score -1}`);
             score = 0;
-            scoreCounter.innerHTML = '';
-            console.log(`hai pescato la bomba ${squareIndex +1}`)
+            scoreCounter.innerHTML = '0000';
+            console.log(`hai pescato la bomba N: ${squareIndex +1}`)
         }
     });
 
@@ -85,7 +87,7 @@ function bombsGenerator(numSquare){
     
     //array che conterrà le bombe
     let bombList = [];
-    console.log(bombList);
+    console.log('Posizione delle bombe', bombList);
 
     //ciclo while 
     while (bombList.length < NUM_BOMBS){
